511bcac3304a1ce591f4c9d14d59c412b41f18a8
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-09-22 10:13:52 +0900

    Get GitHub feed lists

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -3,6 +3,21 @@
     $("#sign_in_github").submit(function(event) {
       event.preventDefault();
       console.log("submit sign in");
+      $.ajax({
+        type: "GET",
+        url: "https://api.github.com/feeds",
+        dataType: "json",
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("Authorization", "Basic " + btoa($("#username").val() + ":" + $("#password").val()));
+        },
+        success: function(data) {
+          console.log("get feed list");
+          console.log(data);
+        },
+        error: function(xhr, status, data) {
+          console.log(data);
+        }
+      });
     });
   }
 }).init();
