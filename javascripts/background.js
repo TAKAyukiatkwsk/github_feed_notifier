1e9feabea1f333eafb25b25d6a308447eeaaccc4
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-09-28 19:00:49 +0900

    Set feed URL

--- javascripts/background.js
+++ javascripts/background.js
@@ -4,14 +4,17 @@
     this.urlChanged();
   },
 
+  feedUrl: "",
+
   urlChanged: function() {
     chrome.storage.onChanged.addListener(function(changes, namespace) {
       for (var key in changes) {
         if (key === "current_user_url") {
           alert("!!!");
           console.log(changes[key].newValue);
+          this.feedUrl = chages[key].newValue;
         }
       }
     });
-  }
+  },
 }).init();
