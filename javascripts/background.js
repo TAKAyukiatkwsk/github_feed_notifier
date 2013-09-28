c985adb13162715e365b9fbccd24a51733765145
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-09-28 18:46:20 +0900

    Create chrome storage onChange listener

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,4 +1,17 @@
 ({
   init: function() {
+    debugger;
+    this.urlChanged();
+  },
+
+  urlChanged: function() {
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+      for (var key in changes) {
+        if (key === "current_user_url") {
+          alert("!!!");
+          console.log(changes[key].newValue);
+        }
+      }
+    });
   }
 }).init();
